15ac03da35063405b8aad8b8a186b26c6b18ac2f
udonn22 <masaki.u1010@gmail.com>
2025-11-23 16:22:01 +0900

    fear: add API

---
+++ api/src/functions/message.js
@@ -0,0 +1,23 @@
+// const { app } = require('@azure/functions');
+
+// app.http('message', {
+//     methods: ['GET', 'POST'],
+//     authLevel: 'anonymous',
+//     handler: async (request, context) => {
+//         context.log(`Http function processed request for url "${request.url}"`);
+
+//         const name = request.query.get('name') || await request.text() || 'world';
+
+//         return { body: `Hello, ${name}!` };
+//     }
+// });
+
+const { app } = require('@azure/functions');
+
+app.http('message', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        return { body: JSON.stringify({ "text": `Hello, from the API!` }) };
+    }
+});
